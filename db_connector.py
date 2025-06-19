c87d3072fd6ea53ea15d7f0c21f752e0489a5d71
Gamovoid17 <66209004+Gamovoid17@users.noreply.github.com>
2025-06-19 12:57:53 +0530

    Update db_connector.py
    password change

--- db_connector.py
+++ db_connector.py
@@ -1,6 +1,6 @@
 import mysql.connector
 
 def get_conn():
-    return mysql.connector.connect(host='localhost', user='root', password='1234',database="credit_card_recommendation")
+    return mysql.connector.connect(host='localhost', user='root', password='password',database="credit_card_recommendation")
 
 
